82bc0d76fa04c1a054748c0e1b3ab4b8bb1d0882
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-25 21:03:17 +0530

    [Amit]  Modified to Delete Data After Value

--- LinkedList/LinkedListOperation.cs
+++ LinkedList/LinkedListOperation.cs
@@ -95,6 +95,20 @@ namespace LinkedList
                 Console.WriteLine("\nLinkedlist is either empty or contains only one element");
         }
 
+        public void DeleteParticularElement(int data)
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                if (temp.next.data == data)
+                {
+                    temp.next = temp.next.next;
+                    Console.WriteLine("\n{0} is Deleted", data);
+                }
+                temp = temp.next;
+            }
+        }
+
         public void SearchVaue(int data)
         {
             Node temp = head;
@@ -145,11 +159,11 @@ namespace LinkedList
 
         public void Display()
         {
-            Console.WriteLine("Displaying Nodes:");
+            Console.WriteLine("\nDisplaying Nodes:");
             Node temp = this.head;
             if (temp == null)
             {
-                Console.WriteLine("LinkedList is Empty");
+                Console.WriteLine("\nLinkedList is Empty");
                 return;
             }
             else
@@ -160,6 +174,17 @@ namespace LinkedList
                     temp = temp.next;
                 }
             }
+        }
+        public void Size()
+        {
+            int size = 0;
+            Node temp = this.head;
+            while (temp != null)
+            {
+                temp = temp.next;
+                size++;
+            }
+            Console.WriteLine("\nSize of the LinkedList is {0}", size);
         }
     }
 }
